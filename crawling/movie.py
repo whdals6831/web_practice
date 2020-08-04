3ab238ba017be16c6fc2eaf2cee0dc1a7cfb5e11
Jeong Jong Min <whdals6831@gmail.com>
2020-08-04 10:33:38 +0900

    2020/08/04 크롤링 약간 수정

--- crawling/movie.py
+++ crawling/movie.py
@@ -8,33 +8,46 @@ def read_json() :
     return movie_data
 
 
-def write_json(movie_data) :
+def write_json(movie) :
     with open('./movie_data.json', 'w', encoding='UTF-8') as json_data:
-        json.dump(movie_data, json_data, ensure_ascii=False, indent=4)
+        json.dump(movie, json_data, ensure_ascii=False, indent=4)
 
-URL = 'https://movie.naver.com/movie/running/current.nhn'
 
-response = requests.get(URL)
+def movie_crawling() :
+    URL = 'https://movie.naver.com/movie/running/current.nhn'
 
-soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get(URL)
 
-current_movie_section = soup.select('div[id=wrap] > div[id=container] > div[id=content] > div[class=article] > div[class=obj_section] > div[class=lst_wrap] > ul[class=lst_detail_t1] > li')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-for movie in current_movie_section :
-    movie_data = {
-        "title" : "",
-        "code" : "",
-    }
+    current_movie_section = soup.select('div[id=wrap] > div[id=container] > div[id=content] > div[class=article] > div[class=obj_section] > div[class=lst_wrap] > ul[class=lst_detail_t1] > li')
 
-    a_tag = movie.select_one('dl > dt > a')
-    movie_data['title'] = a_tag.contents[0]
-    code = a_tag['href']
-    equl_index = code.find('=')+1
-    movie_data['code'] = code[equl_index:]
+    for movie in current_movie_section :
 
-    
-    movie = read_json()
-    movie['movie_data'].append(movie_data)
+        movie_data = {
+            "title" : "",
+            "code" : "",
+        }
 
-    write_json(movie)
-    
+        a_tag = movie.select_one('dl > dt > a')
+        movie_data['title'] = a_tag.contents[0]
+        code = a_tag['href']
+        equl_index = code.find('=')+1
+        movie_data['code'] = code[equl_index:]
+
+        
+        movie = read_json()
+
+        movie_title_list = []
+
+        for movie_dict in movie['movie_data'] :
+             movie_title_list.append(movie_dict['title'])
+        
+        if movie_data['title'] not in movie_title_list :
+            movie['movie_data'].append(movie_data)
+
+        write_json(movie)
+
+
+movie_crawling()
+        
